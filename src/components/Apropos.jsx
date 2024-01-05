e7f708114489fa315e0cb2a63a4f4b9380e909af
JulienR66 <julien-rodriguez66@outlook.fr>
2024-01-05 11:40:05 +0100

    Création compo a propos

--- src/components/Apropos.jsx
+++ src/components/Apropos.jsx
@@ -1,10 +1,19 @@
+import '../styles/Apropos.css';
 import Banner from "./Banner.jsx";
 import aProposImg from "../assets/img-banner-a-propos.png";
+import Topbar from "./Topbar.jsx";
 
 function Apropos() {
+    
     return (
         <>
         <Banner image={aProposImg} title={''} />
+        <div className='app-section'>
+        <Topbar title={'Fiabilité'} contain={'Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.'} />
+        <Topbar title={'Respect'} contain={'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.'} />
+        <Topbar title={'Service'} contain={'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.'} />
+        <Topbar title={'Sécurité'} contain={'La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sacurité établis par nos services. En laissant une note aussi bien à lhote quau locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.'} />
+        </div>
         </>
     )
 }
---
+++ src/components/Topbar.jsx
@@ -0,0 +1,24 @@
+import '../styles/Topbar.css';
+import arrow from '../assets/arrow_back_ios-24px 2.png';
+
+
+function Topbar({title, contain}) {
+
+    return (
+        <>
+        <div className='topbar-container'>
+            <div className='topbar'>
+                <h2>{title}</h2>
+                <img src={arrow} alt="" />
+            </div>
+            <div className='topbar-description'>
+                {contain}
+            </div>
+        </div>
+        </>
+        
+        
+    )
+}
+
+export default Topbar;
