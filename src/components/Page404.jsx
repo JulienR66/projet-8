807dc88b0d228939d7a0956bb1e0b8f926104d81
JulienR66 <julien-rodriguez66@outlook.fr>
2024-01-08 12:55:57 +0100

    media queries

--- src/components/Page404.jsx
+++ src/components/Page404.jsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 
 function Page404() {
     return (
-        <>
+        <div>
         <h1 className='number'>404</h1>
         <p className='message'>Oups! La page que vous demandez n'existe pas.</p>
         <Link className='return' to="/">Retourner sur la page d’accueil</Link>
-        </>
+        </div>
     )
 }
 
