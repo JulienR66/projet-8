bb7119601a0c7e1004a4712a42e992c10134feb8
JulienR66 <julien-rodriguez66@outlook.fr>
2024-01-08 20:56:49 +0100

    changement description + css

--- src/components/Apropos.jsx
+++ src/components/Apropos.jsx
@@ -9,10 +9,10 @@ function Apropos() {
         <>
         <Banner image={aProposImg} title={''} />
         <div className='app-section'>
-        <Topbar title={'Fiabilité'} contain={'Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.'} />
-        <Topbar title={'Respect'} contain={'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.'} />
-        <Topbar title={'Service'} contain={'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.'} />
-        <Topbar title={'Sécurité'} contain={'La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sacurité établis par nos services. En laissant une note aussi bien à lhote quau locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.'} />
+        <Topbar title={'Fiabilité'} contain={<p>Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.</p>} />
+        <Topbar title={'Respect'} contain={<p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</p>} />
+        <Topbar title={'Service'} contain={<p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</p>} />
+        <Topbar title={'Sécurité'} contain={<p>La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hote qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.</p>} />
         </div>
         </>
     )
